4647c25bd631fe61c300d5fdc1d60b127e273104
kiminSong <kiminsee22@gmail.com>
2022-12-13 01:26:45 +0900

    Add :Footer 레이아웃

--- src/pages/Router.js
+++ src/pages/Router.js
@@ -2,16 +2,18 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Home from './Home/Home';
-import Login from './Login/Login';
-import Signup from './Signup/Signup';
+// import Login from './Login/Login';
+// import Signup from './Signup/Signup';
+import Footer from '../components/Footer/Footer';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/f" element={<Footer />} />
+        {/* <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} /> */}
       </Routes>
     </BrowserRouter>
   );
