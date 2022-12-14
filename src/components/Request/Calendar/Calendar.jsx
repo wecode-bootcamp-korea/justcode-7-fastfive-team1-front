d6fc5cb15ddaf4e61ec154723a1efcca396db0d8
SulhwaChoi <imsulhwa00@gmail.com>
2022-12-15 01:38:50 +0900

    ADD : 캘린더 및 대표요청레이아웃

--- src/components/Request/Calendar/Calendar.jsx
+++ src/components/Request/Calendar/Calendar.jsx
@@ -1,23 +1,45 @@
-import { DateRange } from 'react-date-range';
-import { useState } from 'react';
-import 'react-date-range/dist/styles.css'; // main style file
-import 'react-date-range/dist/theme/default.css'; // theme css file
+import React, { useState, useEffect } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { ko } from 'date-fns/esm/locale';
+import moment from 'moment';
+import './react-datepicker.scss';
+import css from './Calendar.module.scss';
 
-function Calender() {
-  const [state, setState] = useState([
-    {
-      startDate: new Date(),
-      endDate: null,
-      key: 'selection',
-    },
-  ]);
+function Calender({ setStartDateData, setEndDateData }) {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  const startDateFormat = moment(startDate, 'YYYY.MM.DD').format('YYYY-MM-DD');
+  const endDateFormat = moment(endDate, 'YYYY.MM.DD').format('YYYY-MM-DD');
+
+  useEffect(() => {
+    setStartDateData(startDateFormat);
+    setEndDateData(endDateFormat);
+  }, [startDate, endDate]);
+
   return (
-    <DateRange
-      editableDateInputs={true}
-      onChange={item => setState([item.selection])}
-      moveRangeOnFirstSelection={false}
-      ranges={state}
-    />
+    <div className={css.calenderBox}>
+      <div className={css.calenderBox}>
+        <DatePicker
+          locale={ko}
+          dateFormat="yyyy년 MM월 dd일"
+          selected={startDate}
+          onChange={date => setStartDate(date)}
+        />
+      </div>
+      <i className="fa-solid fa-arrow-right" />
+
+      <div className={css.calenderBox}>
+        <DatePicker
+          locale={ko}
+          minDate={startDate}
+          dateFormat="yyyy년 MM월 dd일"
+          selected={endDate}
+          onChange={date => setEndDate(date)}
+        />
+      </div>
+    </div>
   );
 }
 
