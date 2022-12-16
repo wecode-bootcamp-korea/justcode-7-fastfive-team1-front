a9b89f1bd82efc1f9f611b17aa8c0efd55d5769f
RBGonion <yopi27@naver.com>
2022-12-16 23:36:00 +0900

    FIX:작업했던 postdetail코드 가져오기

--- src/pages/PostDetail/PostDetail.js
+++ src/pages/PostDetail/PostDetail.js
@@ -4,6 +4,7 @@ import Sidebar from '../../components/Sidebar/Sidebar';
 import WriteReply from '../../components/Comment/WriteReply';
 import Comment from '../../components/Comment/Comment';
 import CommentInput from '../../components/Comment/CommentInput';
+import Header from '../../components/Header/Header';
 
 function PostDetail() {
   const [copyCheck, setCopyCheck] = useState(false);
@@ -19,87 +20,92 @@ function PostDetail() {
 
   return (
     <div className={css.postDetail}>
-      <Sidebar />
-      <div className={`${css.main}`}>
-        <div className={`${css.category} ${css.topDonwMargin}`}>전체 보기</div>
+      <Header />
+      <div className={css.flexDiv}>
+        <Sidebar />
+        <div className={`${css.main}`}>
+          <div className={`${css.category} ${css.topDonwMargin}`}>
+            전체 보기
+          </div>
+          <div className={`${css.imgAndBtnDiv} ${css.topDonwMargin}`}>
+            <div className={`${css.img}`}>img</div>
+            <div className={`${css.btnDiv}`}>
+              <button>수정</button>
+              <div className={`${css.divider}`} />
+              <button>삭제</button>
+            </div>
+          </div>
+          <div
+            className={`${css.title} ${css.fontEmphasis} ${css.topDonwMargin}`}
+          >
+            패스트 파이브
+          </div>
+          <div className={`${css.topDonwMargin}`}>
+            패스트파이브는 일하는 공간을 새롭게 정의합니다. 패스트파이브 오피스
+            플랫폼은 부동산 시장의 수요와 공급을 혁신적으로 통합하며, 공간을
+            채우는 콘텐츠로 기업과 오피스를 연결합니다.
+          </div>
+          <div className={`${css.workField} ${css.topDonwMargin}`}>
+            <div className={`${css.fontEmphasis} ${css.fontEmphasisDiv}`}>
+              업무 분야
+            </div>
+            <div>
+              공유 오피스, 라운지 멤버십, 프리미엄 오피스텔, 사옥 컨설팅
+            </div>
+          </div>
+          <div className={`${css.memberBenefit} ${css.topDonwMargin}`}>
+            <div className={`${css.fontEmphasis} ${css.fontEmphasisDiv}`}>
+              멤버 혜택
+            </div>
+            <div>패스트파이브 멤버 컨택 시 10% 할인 제공</div>
+          </div>
+          <div className={`${css.homepage} ${css.topDonwMargin}`}>
+            <div className={`${css.fontEmphasis} ${css.fontEmphasisDiv}`}>
+              홈페이지
+            </div>
+            <div>https://www.dkgkrltlfgdj.co.kr</div>
+          </div>
+          <div className={`${css.address} ${css.topDonwMargin}`}>
+            <div className={`${css.fontEmphasis} ${css.fontEmphasisDiv}`}>
+              연락처
+            </div>
+            <div className={`${css.EmailAndPhoneNumber}`}>
+              <span className={`${css.Email}`} onClick={clickEmail}>
+                sample@fastfive.co.kr
+              </span>
+              , 010-1234-1234(진양철 회장)
+              {copyCheck && (
+                <div className={`${css.toastDiv}`}>
+                  copy
+                  <i className="fa-solid fa-check" />
+                </div>
+              )}
+            </div>
+          </div>
+          <div className={`${css.topDonwMargin}`}>
+            패스트파이브는 일하는 공간을 새롭게 정의합니다. 패스트파이브 오피스
+            플랫폼은 부동산 시장의 수요와 공급을 혁신적으로 통합하며, 공간을
+            채우는 콘텐츠로 기업과 오피스를 연결합니다. 패스트파이브는 일하는
+            공간을 새롭게 정의합니다. 패스트 파이브 오피스 플랫펌은 부동산
+            시장의 수요와 공급을 혁신적으로 통합하며, 공간을 채우는 콘텐츠로
+            기업과 오피스를 연결합니다. <br />
+            패스트 파이브는 일하는 공간을 새롭게 정의합니다.패스트 파이브는
+            일하는 공간을 새롭게 정의합니다.패스트 파이브는 일하는 공간을 새롭게
+            정의합니다.패스트 파이브는 일하는 공간을 새롭게 정의합니다.
+          </div>
+          <div className={`${css.introduce} ${css.topDonwMargin}`}>
+            <div className={`${css.fontEmphasis} ${css.fontEmphasisDiv}`}>
+              회사 소개서
+            </div>
+            <div>패스트파이브 회사 소개서.pdf</div>
+          </div>
 
-        <div className={`${css.imgAndBtnDiv} ${css.topDonwMargin}`}>
-          <div className={`${css.img}`}>img</div>
-          <div className={`${css.btnDiv}`}>
-            <button>수정</button>
-            <div className={`${css.divider}`} />
-            <button>삭제</button>
+          <div className={`${css.commentDiv}`}>
+            <div className={`${css.commentDivTitle}`}>댓글</div>
+            <CommentInput />
+            <Comment />
+            <WriteReply />
           </div>
-        </div>
-
-        <div
-          className={`${css.title} ${css.fontEmphasis} ${css.topDonwMargin}`}
-        >
-          패스트 파이브
-        </div>
-        <div className={`${css.topDonwMargin}`}>
-          패스트파이브는 일하는 공간을 새롭게 정의합니다. 패스트파이브 오피스
-          플랫폼은 부동산 시장의 수요와 공급을 혁신적으로 통합하며, 공간을
-          채우는 콘텐츠로 기업과 오피스를 연결합니다.
-        </div>
-        <div className={`${css.workField} ${css.topDonwMargin}`}>
-          <div className={`${css.fontEmphasis} ${css.fontEmphasisDiv}`}>
-            업무 분야
-          </div>
-          <div>공유 오피스, 라운지 멤버십, 프리미엄 오피스텔, 사옥 컨설팅</div>
-        </div>
-        <div className={`${css.memberBenefit} ${css.topDonwMargin}`}>
-          <div className={`${css.fontEmphasis} ${css.fontEmphasisDiv}`}>
-            멤버 혜택
-          </div>
-          <div>패스트파이브 멤버 컨택 시 10% 할인 제공</div>
-        </div>
-        <div className={`${css.homepage} ${css.topDonwMargin}`}>
-          <div className={`${css.fontEmphasis} ${css.fontEmphasisDiv}`}>
-            홈페이지
-          </div>
-          <div>https://www.dkgkrltlfgdj.co.kr</div>
-        </div>
-        <div className={`${css.address} ${css.topDonwMargin}`}>
-          <div className={`${css.fontEmphasis} ${css.fontEmphasisDiv}`}>
-            연락처
-          </div>
-          <div className={`${css.EmailAndPhoneNumber}`}>
-            <span className={`${css.Email}`} onClick={clickEmail}>
-              sample@fastfive.co.kr
-            </span>
-            , 010-1234-1234(진양철 회장)
-            {copyCheck && (
-              <div className={`${css.toastDiv}`}>
-                copy
-                <i className="fa-solid fa-check" />
-              </div>
-            )}
-          </div>
-        </div>
-        <div className={`${css.topDonwMargin}`}>
-          패스트파이브는 일하는 공간을 새롭게 정의합니다. 패스트파이브 오피스
-          플랫폼은 부동산 시장의 수요와 공급을 혁신적으로 통합하며, 공간을
-          채우는 콘텐츠로 기업과 오피스를 연결합니다. 패스트파이브는 일하는
-          공간을 새롭게 정의합니다. 패스트 파이브 오피스 플랫펌은 부동산 시장의
-          수요와 공급을 혁신적으로 통합하며, 공간을 채우는 콘텐츠로 기업과
-          오피스를 연결합니다. <br />
-          패스트 파이브는 일하는 공간을 새롭게 정의합니다.패스트 파이브는 일하는
-          공간을 새롭게 정의합니다.패스트 파이브는 일하는 공간을 새롭게
-          정의합니다.패스트 파이브는 일하는 공간을 새롭게 정의합니다.
-        </div>
-        <div className={`${css.introduce} ${css.topDonwMargin}`}>
-          <div className={`${css.fontEmphasis} ${css.fontEmphasisDiv}`}>
-            회사 소개서
-          </div>
-          <div>패스트파이브 회사 소개서.pdf</div>
-        </div>
-
-        <div className={`${css.commentDiv}`}>
-          <div className={`${css.commentDivTitle}`}>댓글</div>
-          <CommentInput />
-          <Comment />
-          <WriteReply />
         </div>
       </div>
     </div>
