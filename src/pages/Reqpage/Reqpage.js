5830e0fd50a9404538fbf64cd8d69b2d4c15cf4d
RBGonion <yopi27@naver.com>
2022-12-20 20:45:26 +0900

    ADD:요청 페이지 API연결

--- src/pages/Reqpage/Reqpage.js
+++ src/pages/Reqpage/Reqpage.js
@@ -19,6 +19,11 @@ function App() {
       .then(data => {
         setUserData(data);
 
+        if (data.userInfo.email === 'admin@gmail.com') {
+          setCurrUserClass('admin');
+          return;
+        }
+
         if (data.userInfo.company.isCompanyMainMember === 0) {
           setCurrUserClass('nomal');
           return;
@@ -26,11 +31,6 @@ function App() {
 
         if (data.userInfo.company.isCompanyMainMember === 1) {
           setCurrUserClass('represent');
-          return;
-        }
-
-        if (data.userInfo.email === 'admin@gmail.com') {
-          setCurrUserClass('admin');
           return;
         }
       });
@@ -61,21 +61,112 @@ function App() {
     }
   }, [currUserClass]);
 
+  const clickMemberSubmitBtn = () => {
+    fetch('http://localhost:5500/member', {
+      method: 'POST',
+      headers: {
+        authorization: localStorage.getItem('authorization'),
+      },
+    });
+  };
+
+  const clickMemberRefuseBtn = () => {
+    fetch('http://localhost:5500/member', {
+      method: 'DELETE',
+      headers: {
+        authorization: localStorage.getItem('authorization'),
+      },
+    });
+  };
+
+  const clickRepresentSubmitBtn = () => {
+    fetch('http://localhost:5500/approve', {
+      method: 'POST',
+      headers: {
+        authorization: localStorage.getItem('authorization'),
+      },
+    });
+  };
+
+  const clickRepresentRefuseBtn = () => {
+    fetch('http://localhost:5500/approve', {
+      method: 'DELETE',
+      headers: {
+        authorization: localStorage.getItem('authorization'),
+      },
+    });
+  };
+
   return (
     <>
       <Header />
       <div className={css.reqpage}>
         <SideBar />
-        <div className={css.main}>
-          {reqData.map(elem => (
-            <div className={css.reqElem} key={elem.id}>
-              <div>{elem.usersId}</div>
-              <div>{elem.companyName}</div>
-              <div>{elem.startDate}</div>
-              <div>{elem.endDate}</div>
+
+        {currUserClass === 'nomal' && (
+          <div className={css.noPermission}>권한이 없습니다</div>
+        )}
+
+        {currUserClass === 'represent' && (
+          <div className={css.memberReqMain}>
+            <div className={css.title}>
+              {reqData[0] && reqData[0].company.companyName} 멤버 요청
             </div>
-          ))}
-        </div>
+            <div className={css.memberReqMainElemDiv}>
+              {reqData.map(elem => (
+                <div className={css.memberReqMainElem} key={elem.id}>
+                  <div>{elem.usersId}</div>
+                  <div>{elem.username}</div>
+                  <div>{elem.email}</div>
+                  <div className={`${css.reqBtnDiv}`}>
+                    <button
+                      className={`${css.reqBtn}`}
+                      onClick={clickMemberSubmitBtn}
+                    >
+                      수락
+                    </button>
+                    <button
+                      className={`${css.reqBtn}`}
+                      onClick={clickMemberRefuseBtn}
+                    >
+                      거절
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+
+        {currUserClass === 'admin' && (
+          <div className={css.representReqMain}>
+            <div className={css.title}>회사 대표 요청</div>
+            <div className={css.representReqMainElemDiv}>
+              {reqData.map(elem => (
+                <div className={css.representReqMainElem} key={elem.id}>
+                  <div>{elem.usersId}</div>
+                  <div>{elem.companyName}</div>
+                  <div>{elem.startDate}</div>
+                  <div>{elem.endDate}</div>
+                  <div className={`${css.reqBtnDiv}`}>
+                    <button
+                      className={`${css.reqBtn}`}
+                      onClick={clickRepresentSubmitBtn}
+                    >
+                      수락
+                    </button>
+                    <button
+                      className={`${css.reqBtn}`}
+                      onClick={clickRepresentRefuseBtn}
+                    >
+                      거절
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
       </div>
       <Footer />
     </>
