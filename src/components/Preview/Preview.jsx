e1566e43b89df34567b1022ad636ac2dbb618044
JIWON CHOI <jgb07012@gmail.com>
2022-12-21 00:30:58 -0500

    MODIFY: css 수정

--- src/components/Preview/Preview.jsx
+++ src/components/Preview/Preview.jsx
@@ -19,6 +19,7 @@ function Preview({ onClose }) {
   }, []);
 
   useEffect(() => {
+    console.log(formData);
     if (formData) {
       let path = formData.companyInfoUrl;
       let idx = path.lastIndexOf('/');
@@ -35,18 +36,16 @@ function Preview({ onClose }) {
       <div className={css.contents}>
         <div className={`${css.imgAndBtnDiv} ${css.topDonwMargin}`}>
           <div className={css.logo}>
-            <img src={formData.companyImgUrl} alt="로고사진" />
+            {formData.companyImgUrl && (
+              <img src={formData.companyImgUrl} alt="로고사진" />
+            )}
           </div>
         </div>
 
         <div className={css.title}>
           <b>{formData.companyName}</b>
         </div>
-        <div className={css.description}>
-          패스트파이브는 일하는 공간을 새롭게 정의합니다. 패스트파이브 오피스
-          플랫폼은 부동산 시장의 수요와 공급을 혁신적으로 통합하며, 공간을
-          채우는 콘텐츠로 기업과 오피스를 연결합니다.
-        </div>
+        <div className={css.description}>{formData.companyShortDesc}</div>
         <div className={`${css.workField} ${css.topDonwMargin}`}>
           <b className={css.menu}>업무 분야 </b> {formData.mainBussinessTags}
         </div>
@@ -65,17 +64,7 @@ function Preview({ onClose }) {
             <b className={css.menu}>연락처</b> {formData.companyContactAddress}
           </div>
         </div>
-        <div className={css.description}>
-          패스트파이브는 일하는 공간을 새롭게 정의합니다. 패스트파이브 오피스
-          플랫폼은 부동산 시장의 수요와 공급을 혁신적으로 통합하며, 공간을
-          채우는 콘텐츠로 기업과 오피스를 연결합니다. 패스트파이브는 일하는
-          공간을 새롭게 정의합니다. 패스트 파이브 오피스 플랫펌은 부동산 시장의
-          수요와 공급을 혁신적으로 통합하며, 공간을 채우는 콘텐츠로 기업과
-          오피스를 연결합니다. <br />
-          패스트 파이브는 일하는 공간을 새롭게 정의합니다.패스트 파이브는 일하는
-          공간을 새롭게 정의합니다.패스트 파이브는 일하는 공간을 새롭게
-          정의합니다.패스트 파이브는 일하는 공간을 새롭게 정의합니다.
-        </div>
+        <div className={css.description}>{formData.companyLongDesc}</div>
         <div className={`${css.introduce} ${css.topDonwMargin}`}>
           <div className={`${css.fontEmphasis} ${css.fontEmphasisDiv}`}>
             <b className={css.menu}>회사 소개서</b>
